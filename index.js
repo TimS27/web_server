fed6490006f6c1f435492c1261ad36f6b643d380
Tim <timschoenwald@googlemail.com>
2022-05-04 12:56:53 +0200

    added different endpoints

--- index.js
+++ index.js
@@ -2,16 +2,52 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "application/json",
-  });
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "Sir Isaac Newton",
-    })
-  );
+const server = http.createServer();
+
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einstein",
+  },
+];
+
+server.on("request", (req, res) => {
+  const items = req.url.split("/"); // /friends/2 => ['', 'friends', '2']
+  if (items[1] === "friends") {
+    // res.writeHead(200, {     // 200 is default
+    //   "Content-Type": "application/json",
+    // });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      const friendIndex = +items[2];
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (items[1] === "/messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac!</li>");
+    res.write("<li>Do you like gravity?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
